86ae929f9856a5b8e383488538e601728202f2ab
atifshehzad668 <151611053+atifshehzad668@users.noreply.github.com>
2024-07-09 22:01:46 +0500

    changes

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -51,7 +51,9 @@
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
                     <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                        <i class="fas fa-expand-arrows-alt"></i>
+                        <i class="fas fa-expand-arrows-alt">
+
+                        </i>
                     </a>
                 </li>
                 <li class="nav-item dropdown">
